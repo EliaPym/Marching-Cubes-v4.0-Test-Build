2daae6f6d4205160047410bd2646c50cbaab68e9
Elia <eliapym@hotmail.co.uk>
2023-06-08 16:46:55 +0100

    Updated shaders
    Added Phong shading to mesh.
    Improved vertex interpolation.
    Fixed shading.

--- src/Resources/Shaders/FragmentShader.glsl
+++ src/Resources/Shaders/FragmentShader.glsl
@@ -5,21 +5,31 @@ in vec3 vertexNormal;
 in vec3 fragPos;
 
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 uniform float ambientStrength;
-uniform float enableDiffuse;
+uniform float specularStrength;
 
 out vec4 fragColour;
 
 void main(){
     vec3 lightColour = vec3(1.0f, 1.0f, 1.0f);
 
+    // ambient lighting
     vec3 ambient = ambientStrength * lightColour;
 
-    vec3 norm = normalize(vertexNormal);
+    // diffuse lighting
+    vec3 norm = -normalize(vertexNormal);
     vec3 lightDir = normalize(lightPos - fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColour * enableDiffuse;
+    vec3 diffuse = diff * lightColour;
 
-    vec3 result = (ambient + diffuse) * vertexColour;
+    // specular lighting
+    vec3 viewDir = normalize(viewPos - fragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColour;
+
+    // phong shading
+    vec3 result = (ambient + diffuse + specular) * vertexColour;
     fragColour = vec4(result, 1.0);
 }
